d4fce867821ea248be93ee10b24e910bff871586
i.davletshin <i.davletshin14@gmail.com>
2021-07-26 01:47:53 +0300

    изменил бургер

--- js/main.js
+++ js/main.js
@@ -1,28 +1,9 @@
-const burgerToggle = document.getElementById('toggle')
-const mainSection = document.querySelector('.main')
+const burger = document.querySelector(".burger");
+const menuList = document.querySelector(".menu__list");
+const body = document.querySelector("body")
 
-burgerToggle.addEventListener('click', () => {
-  if (burgerToggle.checked) {
-    mainSection.classList.add('main-bg')
-  } else {
-    mainSection.classList.remove('main-bg')
-  }
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active')
+  menuList.classList.toggle('active')
+  body.classList.toggle('--scroll-off')
 })
-
-const screenWidth = window.screen.width
-const screenHeight = window.screen.height
-
-const availableScreenWidth = window.screen.availWidth
-const availableScreenHeight = window.screen.availHeight
-
-const windowOuterWidth = window.outerWidth
-const windowOuterHeight = window.outerHeight
-
-const windowInnerWidth = window.innerWidth
-const windowInnerHeight = window.innerHeight
-
-const pageWidth = document.documentElement.scrollWidth
-const pageHeight = document.documentElement.scrollHeight
-
-console.log(pageWidth);
-console.log(pageHeight);
