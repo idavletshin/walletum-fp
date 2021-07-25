aad29789640a0521767c103042d51f19fcf982ae
i.davletshin <i.davletshin14@gmail.com>
2021-07-25 15:19:21 +0300

    test responsive

--- js/main.js
+++ js/main.js
@@ -8,3 +8,21 @@ burgerToggle.addEventListener('click', () => {
     mainSection.classList.remove('main-bg')
   }
 })
+
+const screenWidth = window.screen.width
+const screenHeight = window.screen.height
+
+const availableScreenWidth = window.screen.availWidth
+const availableScreenHeight = window.screen.availHeight
+
+const windowOuterWidth = window.outerWidth
+const windowOuterHeight = window.outerHeight
+
+const windowInnerWidth = window.innerWidth
+const windowInnerHeight = window.innerHeight
+
+const pageWidth = document.documentElement.scrollWidth
+const pageHeight = document.documentElement.scrollHeight
+
+console.log(pageWidth);
+console.log(pageHeight);
