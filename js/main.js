57a0f26bfdd45cdc061f90f9514d18a206844d78
i.davletshin <i.davletshin14@gmail.com>
2021-08-02 18:04:24 +0300

    второй шаг

--- js/main.js
+++ js/main.js
@@ -2,14 +2,8 @@ const burger = document.querySelector(".burger");
 const menuList = document.querySelector(".menu__list");
 const body = document.querySelector("body")
 
-const seedWallet = document.querySelector(".wallet-card.--yellow-border")
-
 burger.addEventListener('click', () => {
   burger.classList.toggle('active')
   menuList.classList.toggle('active')
   body.classList.toggle('--scroll-off')
-})
-
-seedWallet.addEventListener('click', () => {
-  window.location.href = 'step3.html';
 })
