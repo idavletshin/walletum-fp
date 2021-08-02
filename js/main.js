ec49327985f979400f68d5ddc645f6667af6d2aa
i.davletshin <i.davletshin14@gmail.com>
2021-08-02 13:15:11 +0300

    переходы между страницами

--- js/main.js
+++ js/main.js
@@ -2,8 +2,14 @@ const burger = document.querySelector(".burger");
 const menuList = document.querySelector(".menu__list");
 const body = document.querySelector("body")
 
+const seedWallet = document.querySelector(".wallet-card.--yellow-border")
+
 burger.addEventListener('click', () => {
   burger.classList.toggle('active')
   menuList.classList.toggle('active')
   body.classList.toggle('--scroll-off')
+})
+
+seedWallet.addEventListener('click', () => {
+  window.location.href = '../step3.html';
 })
