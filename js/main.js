689b01ce35cf44e0f278b77d09215d0359af2c60
i.davletshin <i.davletshin14@gmail.com>
2021-08-17 21:15:28 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 const burger = document.querySelector(".burger");
 const menuList = document.querySelector(".menu__list");
 const body = document.querySelector("body")
+const showHidePasswd = document.querySelector(".text-field__icon");
+const passwd = document.querySelector(".password");
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('active')
   menuList.classList.toggle('active')
   body.classList.toggle('--scroll-off')
 })
+
+showHidePasswd.addEventListener("click", function () {
+  if (passwd.getAttribute("type") == "password") {
+    showHidePasswd.classList.remove("view");
+    passwd.setAttribute("type", "text");
+  } else {
+    showHidePasswd.classList.add("view");
+    passwd.setAttribute("type", "password");
+  }
+});
+
+// window.function(){
+//   document.getElementsByTagName("body")[0].style.height = window.innerHeight + 'px';
+// }
