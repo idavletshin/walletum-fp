139a056a2bf32473f9f412791e1ae9b52741b6c2
i.davletshin <i.davletshin14@gmail.com>
2021-08-19 22:21:29 +0300

    entry seed wallet

--- js/main.js
+++ js/main.js
@@ -2,21 +2,8 @@ const burger = document.querySelector(".burger");
 const menuList = document.querySelector(".menu__list");
 const body = document.querySelector("body")
 const togglePasswordList = document.querySelectorAll(".text-field__icon");
+// const pastePassword = document.querySelector(".text-field__paste");
 const typeList = document.querySelectorAll(".password");
-// const document = document.documentElement;
-
-/* Function to open fullscreen mode */
-// function openFullscreen() {
-//   if (document.requestFullscreen) {
-//     document.requestFullscreen();
-//   } else if (document.mozRequestFullScreen) { /* Firefox */
-//     document.mozRequestFullScreen();
-//   } else if (document.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
-//     document.webkitRequestFullscreen();
-//   } else if (document.msRequestFullscreen) { /* IE/Edge */
-//     document.msRequestFullscreen();
-//   }
-// }
 
 window.addEventListener("load", function () {
   setTimeout(function () {
@@ -39,9 +26,3 @@ for (let i = 0; i < togglePasswordList.length; i++) {
     this.classList.toggle("view");
   });
 }
-
-
-
-// window.function(){
-//   document.getElementsByTagName("body")[0].style.height = window.innerHeight + 'px';
-// }
